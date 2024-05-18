cb9d2090fd7b5d587f1dd80f83c3d7414b978595
mohamed <mohamedaprahym@gmail.com>
2024-05-18 18:26:46 +0300

    s

--- models/base_model.py
+++ models/base_model.py
@@ -15,8 +15,8 @@ class BaseModel:
         __init__ method
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
     
     def save(self):
         """
