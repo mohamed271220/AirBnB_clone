dd2361ab1ff4981a028654064aa9e8a20ce157f4
mohamed <mohamedaprahym@gmail.com>
2024-05-18 18:34:19 +0300

    s

--- models/base_model.py
+++ models/base_model.py
@@ -4,19 +4,27 @@ base model
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """
-    BaseModel class
-    """
-    def __init__(self):
-        """
-        __init__ method
-        """
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+
+        models.storage.new(self)
+
     
     def save(self):
         """
