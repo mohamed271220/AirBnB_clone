c52acb46c9fe07acf72a8c032de4f478cefd8315
mohamed <mohamedaprahym@gmail.com>
2024-05-19 09:34:54 +0300

    s

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
     """
     BaseModel class
     """
-     def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
