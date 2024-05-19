22d61ef24972db7046eb61210eca5f156e0abd4d
mohamed <mohamedaprahym@gmail.com>
2024-05-19 09:21:06 +0300

    s

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import cmd
 import shlex
 from models.base_model import BaseModel
 from models import storage
+import re
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -43,12 +44,13 @@ class HBNBCommand(cmd.Cmd):
         """
         Create command to create a new instance of BaseModel
         """
-        if not line:
+        command = shlex.split(line)
+        if len(command) == 0:
             print("** class name missing **")
-        elif line not in self.classes:
+        elif command[0] not in self.classes:
             print("** class doesn't exist **")
         else:
-            new_instance = eval(line)()
+            new_instance = BaseModel()
             new_instance.save()
             print(new_instance.id)
 
@@ -90,6 +92,80 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
             else:
                 print("** no instance found **")
+
+    def do_all(self, line):
+        """
+        All command to print all string representation of all instances
+        """
+        args = shlex.split(line)
+        all_objs = storage.all()
+        if not line:
+            print([str(value) for value in all_objs.values()])
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            print([str(value) for key, value in all_objs.items() if args[0] in key])
+    
+    def do_update(self, arg):
+        """
+        Update an instance by adding or updating an attribute.
+        """
+        commands = shlex.split(arg)
+
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(commands) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+
+            key = "{}.{}".format(commands[0], commands[1])
+            if key not in objects:
+                print("** no instance found **")
+            elif len(commands) < 3:
+                print("** attribute name missing **")
+            elif len(commands) < 4:
+                print("** value missing **")
+            else:
+                obj = objects[key]
+                curly_braces = re.search(r"\{(.*?)\}", arg)
+
+                if curly_braces:
+                    try:
+                        str_data = curly_braces.group(1)
+
+                        arg_dict = ast.literal_eval("{" + str_data + "}")
+
+                        attribute_names = list(arg_dict.keys())
+                        attribute_values = list(arg_dict.values())
+                        try:
+                            attr_name1 = attribute_names[0]
+                            attr_value1 = attribute_values[0]
+                            setattr(obj, attr_name1, attr_value1)
+                        except Exception:
+                            pass
+                        try:
+                            attr_name2 = attribute_names[1]
+                            attr_value2 = attribute_values[1]
+                            setattr(obj, attr_name2, attr_value2)
+                        except Exception:
+                            pass
+                    except Exception:
+                        pass
+                else:
+
+                    attr_name = commands[2]
+                    attr_value = commands[3]
+
+                    try:
+                        attr_value = eval(attr_value)
+                    except Exception:
+                        pass
+                    setattr(obj, attr_name, attr_value)
+
+                obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
