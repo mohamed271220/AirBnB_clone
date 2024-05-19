d9f70f19a90f1f3c377d39c999cca658f55513a6
mohamed <mohamedaprahym@gmail.com>
2024-05-19 07:22:43 +0300

    s

--- models/base_model.py
+++ models/base_model.py
@@ -9,13 +9,23 @@ class BaseModel:
     """
     BaseModel class
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         __init__ method
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                elif key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
     
     def save(self):
         """
